be4c26f56f7b7edd58849b27904d482dabbcfc64
Manas <manas@192.168.1.7>
2026-05-10 15:23:39 +0530

    fixed homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import Dashboard from "./components/Dashboard";
-import BMI from "./components/BMI";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Workout from "./components/Workout";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-        <Route path="/workout" element={<Workout />} />
-
-        <Route path="/bmi" element={<BMI />} />
-
-        <Route path="/dashboard" element={<Dashboard />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
+        <Route path="/" element={<Dashboard />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
